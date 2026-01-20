b5c120017815102a6bc25084a308639314d3f86d
vazquezlilen78-jpg <vazquezlilen78@gmail.com>
2026-01-20 17:15:52 -0300

    script.js

--- script.js
+++ script.js
@@ -8,18 +8,24 @@ btn.addEventListener("click", () => {
 
 // Cuenta regresiva
 const eventoFecha = new Date("Feb 20, 2026 18:00:00").getTime();
-const reloj = document.getElementById("reloj");
+const reloj = document.getElementById("reloj");  // asegúrate que coincide con el id
 
 setInterval(() => {
     const ahora = new Date().getTime();
     const distancia = eventoFecha - ahora;
+
+    if(distancia < 0){
+        reloj.innerHTML = "¡Es hoy! 🎉";
+        return;
+    }
+
     const dias = Math.floor(distancia / (1000*60*60*24));
     const horas = Math.floor((distancia % (1000*60*60*24)) / (1000*60*60));
     const minutos = Math.floor((distancia % (1000*60*60)) / (1000*60));
     const segundos = Math.floor((distancia % (1000*60)) / 1000);
+
     reloj.innerHTML = ${dias}d ${horas}h ${minutos}m ${segundos}s;
 }, 1000);
-
 // RSVP
 document.getElementById("formulario").addEventListener("submit", e => {
     e.preventDefault();
